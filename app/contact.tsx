4e58d0fbadd71b9432a25fa44a11c98863934ac5
RedwansHub <redwanmuhiadin@outlook.com>
2024-05-09 04:48:42 +0300

    v1.01

--- app/contact.tsx
+++ app/contact.tsx
@@ -26,7 +26,7 @@ const Contact = (props: Props) => {
         setTimeout(() => {
           setLoading(false);
           setTransfer(!transfer); // Switch to "Done" state
-        }, 1000); // 1 second for loading animation
+        }, 2000); // 1 second for loading animation
       };
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -51,94 +51,85 @@ const Contact = (props: Props) => {
                 animate={{ opacity: 1, y: 0}}
                 transition={{ duration: 1, delay:1}} 
                 onClick={() => setOpen(!open)} 
-                className='border p-4 bg-black/20 hover:scale-110 cursor-pointer hover:bg-black/60 rounded-xl duration-500 ease-in-out'>
-                <div className='font-semibold text-xl p-4'>
+                className='border px-4 bg-black/20 hover:scale-110 cursor-pointer hover:bg-black/60 rounded-xl duration-500 ease-in-out'>
+                <div className='font-semibold text-lg p-4'>
                     Get in touch with us.
                 </div>
             </motion.div>
         </div>
             
             {/* <div className={'w-full h-full flex'}> */}
-            <div className={!open ? 'w-full h-full flex   \
-            p-8' 
+            <div className={!open ? 'w-full h-full flex p-8' 
                 : 'hidden'}>
                     <div className='w-full flex flex-end justify-end'>
                         <motion.div 
-                        className={ ` w-fit p-4  rounded-3xl shadow-2xl  border flex  justify-end  relative items-center 
+                        
+                        initial={open ? 
+                            {opacity: 0, scale: 0.2 } :
+                            {opacity: 0, scale: 0.2 } }
+                        animate={ open ?
+                            {opacity: 0, scale: 1 } : 
+                            {opacity: 1, scale: 1 } } 
+                        transition={{ duration : 2}}
+                        className={ `  px-4 rounded-3xl transition-all duration-300 ease-in-out [450px] shadow-2xl   flex  justify-end  relative items-center 
                             ${open ? 'backgoundBlur2' : 'bg-black/20 shadow-xl bg-opacity-15' }`}    >
-                            <div 
-                            // animate={open ? {opacity: 0, y:-80 } : { opacity: 1, y:0 }} transition={{ duration: 1, }} 
-                            className={'h-fit  w-[450px] flex flex-col gap-4'}>
-                                <div>
-                                    <h2>Write us a message.</h2>
-                                </div>
-                                <div className='w-full relative overflow-hidden h-full  '>
-                                    <motion.div  
-                                        animate={open ? {opacity: 0, x: 400 } : { opacity: 1, x:0 }} transition={{ duration: 1}}
-                                        className='flex flex-col gap-2'>
-                                        <div
-                                        className='font-light text-lg border border-white/40 p-2 bg-black/10 shadow-2xl cursor-pointer hover:bg-black/60 rounded-xl duration-500 ease-in-out'>
-                                        <input  type="text" name="name"   onChange={() => handleChange} onClick={(e)=> handleClick('name', e)} placeholder='Full Name' className='w-full placeholder:font-semibold p-2 outline-none bg-transparent'/>
-                                            
+                                {loading && (
+                                    <div className='w-full   absolute top-0 rounded-t-3xl right-0 backdrop-blur-lg z-20 bg-green-400/40 rounded-sm flex justify-center items-center  h-[10%]'>
+                                            <h2 className='text-xl font-bold'>Thank you for messaging us.</h2>
+                                    </div>
+                                )}
+                                <form onSubmit={handleSubmit}
+                                    // animate={open ? {opacity: 0, y:-80 } : { opacity: 1, y:0 }} transition={{ duration: 1, }} 
+                                    className={'h-fit  w-[450px] flex flex-col gap-4 px-4'}>
+                                        <div className='py-4'>
+                                            <h2 className='font-bold text-2xl'>Write us a message.</h2>
                                         </div>
-                                        {errors.name && <span className="error">{errors.name}</span>}
-                                    </motion.div>
-                                </div>
-                                <div className='w-full relative overflow-hidden h-full  '>
-                                    <motion.div 
-                                        animate={open ? {opacity: 0, x: 400 } : { opacity: 1, x:0 }} transition={{ duration: 1, delay: 0.2}}                             
-                                        className='flex flex-col gap-2 '
-                                        >
-                                        {/* <label className='font-semibold text-white text-xl tracking-wider'>Full Name</label> */}
-                                            <div
-                                        className='font-light text-lg border border-white/40 p-2 bg-black/10 shadow-2xl cursor-pointer hover:bg-black/60 rounded-xl '>
-                                                <input  type="text" name="email"  onChange={() => handleChange} onClick={(e)=> handleClick('name', e)} placeholder='Email' className='w-full placeholder:font-semibold p-2   outline-none bg-transparent'/>
-                                            </div>
-                                        {errors.email && <span className="error">{errors.email}</span>}
-                                    </motion.div>
-                                </div>
-                                <div className='w-full relative overflow-hidden h-full  '>
-                                    <motion.div   
-                                        animate={open ? {opacity: 0, y: 100 } : { opacity: 1, y:0 }} transition={{ duration: 1}}                             
-                                            className='font-light text-lg border border-white/40 p-2 bg-black/10 shadow-2xl hover:scale-110 cursor-pointer hover:bg-black/60 rounded-xl '>                                        
-                                        <textarea  name="message"  onChange={() => handleChange} onClick={(e)=> handleClick('name', e)} placeholder='Message' className='w-full placeholder:font-semibold p-2 outline-none bg-transparent'/>
-                                        {errors.message && <span className="error">{errors.message}</span>}
-                                    </motion.div>
-                                </div>
-                                <div className='w-full relative overflow-hidden h-full  '>
-                                    <motion.div 
-                                        animate={open ? {opacity: 0, y: 100 } : { opacity: 1, x:0 }} transition={{ duration: 1, }}       
-                                        onClick={() => handleSubmit} className='transition-all border p-4 hover:bg-opacity-80 rounded-xl cursor-pointer hover:bg-black/50 '>
-                                        
-                                        {transfer ?
-                                            <div className=''>
-                                                <motion.h2 
-                                                    initial={{ x: 80}} 
-                                                    animate={{ x: 0}}
-                                                    exit={{  x: -80 }}
-                                                    transition={{ duration: 1}} 
-                                                    className='font-semibold text-xl text-center '>Sending</motion.h2>
-                                            </div>
-                                            :
-                                            <motion.div >
-                                                <motion.h2 
-                                                    initial={{  x: 80}} 
-                                                    animate={{  x: 0}}
-                                                    exit={{  x: -80 }}
-                                                    transition={{ duration: 1}} 
-                                                    className='font-semibold text-xl text-center'>
-                                                        Send Message
-                                                    </motion.h2>
+                                        <div className='w-full relative overflow-hidden h-full  '>
+                                            <motion.div  
+                                                animate={open ? {opacity: 0, x: 400 } : { opacity: 1, x:0 }} transition={{ duration: 1}}
+                                                className='flex flex-col gap-2'>
+                                                <div
+                                                className='font-light text-lg border border-white/40 p-2 bg-black/10 shadow-2xl cursor-pointer hover:bg-black/60 rounded-xl duration-500 ease-in-out'>
+                                                <input  type="text" name="name"   onChange={() => handleChange} onClick={(e)=> handleClick('name', e)} placeholder='Full Name' className='w-full placeholder:font-semibold p-2 outline-none bg-transparent'/>
+                                                    
+                                                </div>
+                                                {errors.name && <span className="error">{errors.name}</span>}
                                             </motion.div>
-                                        }
-                                    </motion.div>
-                                </div>
-                                {/* <motion.div  
-                                className=' '>
-                                </motion.div> */}
-                               
+                                        </div>
+                                        <div className='w-full relative overflow-hidden h-full  '>
+                                            <motion.div 
+                                                animate={open ? {opacity: 0, x: 400 } : { opacity: 1, x:0 }} transition={{ duration: 1, delay: 0.2}}                             
+                                                className='flex flex-col gap-2 '
+                                                >
+                                                {/* <label className='font-semibold text-white text-xl tracking-wider'>Full Name</label> */}
+                                                    <div
+                                                className='font-light text-lg border border-white/40 p-2 bg-black/10 shadow-2xl  cursor-pointer hover:bg-black/60 rounded-xl '>
+                                                        <input  type="text" name="email" required  onChange={() => handleChange} onClick={(e)=> handleClick('name', e)} placeholder='Email' className='w-full placeholder:font-semibold p-2 required:border-red-500   outline-none bg-transparent'/>
+                                                    </div>
+                                                {errors.email && <span className="error">{errors.email}</span>}
+                                            </motion.div>
+                                        </div>
+                                        <div className='w-full relative overflow-hidden h-full  '>
+                                            <motion.div   
+                                                animate={open ? {opacity: 0, y: 100 } : { opacity: 1, y:0 }} transition={{ duration: 1}}                             
+                                                    className='font-light text-lg border border-white/40 p-2 bg-black/10 shadow-2xl hover:scale-110 cursor-pointer hover:bg-black/60 rounded-xl '>                                        
+                                                <textarea  name="message"  onChange={() => handleChange} onClick={(e)=> handleClick('name', e)} placeholder='Message' className='w-full placeholder:font-semibold p-2 outline-none bg-transparent'/>
+                                                {errors.message && <span className="error">{errors.message}</span>}
+                                            </motion.div>
+                                        </div>
+                                        <div className='w-full relative overflow-hidden h-full  '>
+                                            <motion.div 
+                                                animate={open ? {opacity: 0, y: 100 } : { opacity: 1, x:0 }} transition={{ duration: 1, }}       
+                                                className=''>
+                                                    <button onSubmit={() => handleSubmit} 
+                                                    className='transition-all border-blue-500 border-2 w-full text-center text-2xl font-bold p-4 hover:bg-opacity-80 rounded-xl cursor-pointer hover:bg-black/50 '>
+                                                        Send
+                                                    </button>
+                                            </motion.div>
+                                        </div>
 
-                            </div>
+                                    </form>
+                                
                         </motion.div>
                     </div>
                 
